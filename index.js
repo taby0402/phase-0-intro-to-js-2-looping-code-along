f9ade96f83bc433089ea6d3cca5e44bb729160e3
Tabitha <flor.palida@yahoo.com>
2023-06-02 14:18:24 -0400

    corrected errors

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+function writeCards(stringArray, eventName) {
+    let messages = [];
+    for (let i = 0; i < stringArray.length; i++) {
+messages.push(`Thank you, ${stringArray[i]}, for the wonderful ${eventName} gift!`);
+}
+    return messages;
+}
+function countDown(down) {
+while (down >= 0) {
+    console.log(down--);
+}
+}
